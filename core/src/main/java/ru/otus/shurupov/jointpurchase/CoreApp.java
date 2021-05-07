79f70748eb889dee58c1cc8ad4d1a9f25909f37e
Evgeny Shurupov <bolteg86@ya.ru>
2021-05-07 18:04:02 +0400

    2 Added swagger

--- core/src/main/java/ru/otus/shurupov/jointpurchase/CoreApp.java
+++ core/src/main/java/ru/otus/shurupov/jointpurchase/CoreApp.java
@@ -2,8 +2,10 @@ package ru.otus.shurupov.jointpurchase;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class CoreApp {
     public static void main(String[] args) {
         SpringApplication.run(CoreApp.class, args);
