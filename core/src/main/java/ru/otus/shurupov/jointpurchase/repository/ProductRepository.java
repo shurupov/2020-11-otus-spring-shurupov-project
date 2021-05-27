a09bc3e235e19ab8a6c969380c2daca09b0d629e
Evgeny Shurupov <bolteg86@ya.ru>
2021-05-28 00:37:25 +0400

    10 Tuned Gateway

--- core/src/main/java/ru/otus/shurupov/jointpurchase/repository/ProductRepository.java
+++ core/src/main/java/ru/otus/shurupov/jointpurchase/repository/ProductRepository.java
@@ -1,9 +1,7 @@
 package ru.otus.shurupov.jointpurchase.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import ru.otus.shurupov.jointpurchase.domain.Product;
 
-@CrossOrigin
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
--- core/src/main/java/ru/otus/shurupov/jointpurchase/repository/UserRepository.java
+++ core/src/main/java/ru/otus/shurupov/jointpurchase/repository/UserRepository.java
@@ -1,14 +1,10 @@
 package ru.otus.shurupov.jointpurchase.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.web.bind.annotation.CrossOrigin;
 import ru.otus.shurupov.jointpurchase.domain.User;
 
 import java.util.Optional;
 
-@CrossOrigin
 public interface UserRepository extends JpaRepository<User, Long> {
-
     Optional<User> getByUsername(String username);
-
 }
